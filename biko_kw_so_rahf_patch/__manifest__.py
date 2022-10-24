fd87bd7b3329802d424c2b243f09bd2cff6bec15
Artem Borovlev <bas@qdes.com.ua>
2022-10-25 02:07:30 +0300

    создание модуля, восстановление шаблона kw_so_rahf

---
+++ biko_kw_so_rahf_patch/__manifest__.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "BIKO: Патч печатной формы KW_SO_RAHF",
+    'version': '14.0.1.1.0',
+    'author': 'Borovlev A.S.',
+    'company': 'Quick Decisions',
+    "depends": ['kw_so_rahf'],
+    "data": [
+    ],
+    
+    'license': 'LGPL-3',
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+}
