2045f02c7d5207b27eacea4a8c39c1c407c3effb
Artem Borovlev <bas@qdes.com.ua>
2022-10-25 01:10:09 +0300

    добавлены печатные формы от KW добавлено расширение валют

---
+++ kw_invoice_akt/__manifest__.py
@@ -0,0 +1,26 @@
+{
+    'name': 'Akt Vikonanih Robit Invoice for Ukraine',
+    'summary': 'Akt vykonanyh robit Акт виконатих робіт',
+
+    'author': 'Kitworks Systems',
+    'website': 'https://kitworks.systems/',
+
+    'category': 'Customizations',
+    'license': 'OPL-1',
+    'version': '14.0.1.0.1',
+
+    'depends': ['account', 'kw_invoice_doc_base'],
+
+    'data': [
+        'report/report.xml',
+        'report/templates.xml',
+    ],
+
+    'installable': True,
+
+    'images': [
+        'static/description/cover.png',
+        'static/description/icon.png',
+    ],
+
+}
---
+++ kw_so_doc_base/models/__init__.py
@@ -0,0 +1,5 @@
+from . import (
+    product_template,
+    sale_order,
+    sale_order_line,
+)
