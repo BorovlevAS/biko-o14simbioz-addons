b075d6a70d854307f62222c26840621661393b10
Borovlev A. S <bas@qdes.com.ua>
2022-11-08 23:31:48 +0200

    адаптация модуля под 14.0

--- kw_bank_import_base/models/__init__.py
+++ kw_bank_import_base/models/__init__.py
@@ -2,5 +2,5 @@ from . import account
 from . import statement_import
 from . import account_bank_statement
 from . import account_bank_statement_import
-from . import partner
+# from . import partner
 from . import sequence_mixin
