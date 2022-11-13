91b7315c77f68d5dc2721feea9fa074bc475b79e
Borovlev A. S <bas@qdes.com.ua>
2022-11-13 20:00:32 +0200

    добавил ПФ акта, добавил выбор фоп/тов

--- biko_invoice_rahunok_pf/__manifest__.py
+++ biko_invoice_rahunok_pf/__manifest__.py
@@ -1,14 +1,16 @@
 # -*- coding: utf-8 -*-
 {
     'name': "BIKO: Печатная форма Счет для документа Invoice",
-    'version': '14.0.1.1.0',
+    'version': '14.0.1.1.1',
     'author': 'Borovlev A.S.',
     'company': 'BIKO Solutions',
     "depends": ['account', 'biko_stamp_signature', 'kw_account_partner_requisites'],
     "data": [
         'views/account_move_views.xml',
         'reports/rahunok_template.xml',
-        'views/actions.xml'
+        'reports/report_akt_template.xml',
+        'views/actions.xml',
+        'views/actions_akt.xml',
     ],
     
     'license': 'LGPL-3',
